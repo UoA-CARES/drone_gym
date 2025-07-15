935f9eb28074e6a98f9e3d211e8953de91196c38
jamesz3ng <jameszeng23@gmail.com>
2025-07-15 22:16:00 +0200

    feat: implement reset

--- environment.py
+++ environment.py
@@ -1,1 +1,36 @@
+from drone import Drone
+import time
 
+class DroneEnvironment:
+
+    def __init__(self):
+        self.drone = Drone()
+        self.reset_position = [0, 0, 1]
+        self.reward = 0.0
+        self.done = False
+
+    def reset(self):
+        # Check that the drone is not already flying
+        self.drone.set_target_position(self.reset_position[0], self.reset_position[1], self.reset_position[2])
+
+
+        if self.drone.is_flying_event.is_set():
+            print("Control: The drone is already flying")
+            self.drone.land()
+            self.drone.is_landed_event.wait(timeout=30)
+            self.drone.stop()
+
+        self.drone.take_off()
+        self.drone.is_flying_event.wait(timeout=15)
+        self.drone.start_position_control()
+        time.sleep(10)
+
+    def step(self, action):
+        # to do
+
+
+
+
+if __name__ == "__main__":
+    env = DroneEnvironment()
+    env.reset()
