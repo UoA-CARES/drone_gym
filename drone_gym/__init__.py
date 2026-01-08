1d3c8ab57214bb5efb1e20262260b1e0ea88ca1f
Arushi Gupta <agup537@aucklanduni.ac.nz>
2026-01-09 12:14:53 +1300

    move tasks to drone_gym

--- drone_gym/__init__.py
+++ drone_gym/__init__.py
@@ -1,5 +1,17 @@
 from drone_gym.drone_environment import DroneEnvironment
 from drone_gym.move_to_2d_position import MoveToPosition
 from drone_gym.move_to_random_2d_position import MoveToRandomPosition
+from drone_gym import (
+    move_to_2d_position,
+    move_to_random_2d_position,
+    move_to_3d_position,
+    move_to_random_3d_position,
+)
 
 __all__ = ['DroneEnvironment', 'MoveToPosition', 'MoveToRandomPosition']
+task_map = {
+    "move_to_2d_position": move_to_2d_position.MoveToPosition,
+    "move_to_random_2d_position": move_to_random_2d_position.MoveToRandomPosition,
+    "move_to_3d_position": move_to_3d_position.MoveTo3DPosition,
+    "move_to_random_3d_position": move_to_random_3d_position.MoveToRandom3DPosition,
+}
