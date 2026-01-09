735dc11a3a98af6337c27ce4647151c5e92f0c11
Arushi Gupta <agup537@aucklanduni.ac.nz>
2026-01-09 13:52:15 +1300

    use task_factory for selecting task

--- drone_gym/__init__.py
+++ drone_gym/__init__.py
@@ -1,17 +1,5 @@
 from drone_gym.drone_environment import DroneEnvironment
 from drone_gym.move_to_2d_position import MoveToPosition
 from drone_gym.move_to_random_2d_position import MoveToRandomPosition
-from drone_gym import (
-    move_to_2d_position,
-    move_to_random_2d_position,
-    move_to_3d_position,
-    move_to_random_3d_position,
-)
 
 __all__ = ['DroneEnvironment', 'MoveToPosition', 'MoveToRandomPosition']
-task_map = {
-    "move_2d": move_to_2d_position.MoveToPosition,
-    "move_2d_random": move_to_random_2d_position.MoveToRandomPosition,
-    "move_3d": move_to_3d_position.MoveTo3DPosition,
-    "move_3d_random": move_to_random_3d_position.MoveToRandom3DPosition,
-}
