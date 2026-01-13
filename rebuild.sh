33d7da076f2845deb4202be90386d9f92ce3b4c8
Nate Williamson <nwil508@aucklanduni.ac.nz>
2026-01-14 11:26:01 +1300

    improve rebuild.sh to accept cli arg for base dir

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,10 @@
 # This script rebuilds the CrazySim simulator because builds are flakey and often fail silently, needing to be rebuilt.
-
-echo "//" >> ../CrazySim/crazyflie-firmware/tools/crazyflie-simulation/simulator_files/gazebo/plugins/CrazySim/crazysim_plugin.cpp # Bypass caching
+# Usage: bash rebuild.sh <base_directory>
+base_dir="$1"
 current_dir=$(pwd)
-cd ../CrazySim/crazyflie-firmware # Simulator (Gazebo)
+
+cd $base_dir/CrazySim/crazyflie-firmware # Simulator (Gazebo)
+echo "//" >> tools/crazyflie-simulation/simulator_files/gazebo/plugins/CrazySim/crazysim_plugin.cpp # Bypass caching
 mkdir -p sitl_make/build && cd $_
 cmake ..
 make all
